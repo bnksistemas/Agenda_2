8819ae2cf5db6180367374b16c0f5e347060e97b
bnksistemas <gpfefferman@gmail.com>
2023-11-09 19:34:36 -0300

    Video 10 terminando el crud

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, agregar_contacto, listar_contactos
+from .views import home, agregar_contacto, listar_contactos, modificar_contacto, eliminar_contacto
 
 urlpatterns = [
     path('', home, name="home"),
     path('agregar-contacto/', agregar_contacto, name="agregar_contacto"),
     path('listar-contactos/', listar_contactos, name="listar_contactos"),
-   
+    path('modificar-contacto/<id>/', modificar_contacto, name="modificar_contacto"),
+    path('eliminar-contacto/<id>/', eliminar_contacto, name="eliminar_contacto"),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AgendaForm
 from .models import Agenda
 
@@ -28,3 +28,33 @@ def listar_contactos(request):
     }
 
     return render(request,'agenda/listar_contactos.html', data)
+
+
+def modificar_contacto(request, id):
+
+    contacto = get_object_or_404(Agenda, id=id)
+
+    data = {
+        'form': AgendaForm(instance=contacto)
+    }
+
+    if request.method == 'POST':
+        formulario = AgendaForm(
+            data=request.POST,
+            instance=contacto,
+            files=request.FILES
+        )
+
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to='listar_contactos')
+        data['form'] = formulario
+        data['mensaje'] = formulario.errors
+
+    return render(request, 'agenda/modificar_contacto.html', context=data)
+
+def eliminar_contacto(request, id):
+    contacto =get_object_or_404(Agenda, id=id)
+    contacto.delete()
+
+    return redirect(to='listar_contactos')
